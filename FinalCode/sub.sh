5ef66034a4d1c3734bd6d70ac1c762bf79cacb80
Jahnavi <112300327+JahnaviUTA@users.noreply.github.com>
2023-04-16 23:07:59 -0500

    Add files via upload
    Put these files in the yolov7 directory

--- FinalCode/sub.sh
+++ FinalCode/sub.sh
@@ -1,6 +1,6 @@
 # detect.py
-executable      = track_car_lp_new.py
-arguments       = "--conf-thres 0.25 --source ../test/20230222_115854.mp4 --device 0 --classes 1 2 3 5 7 --save-crop-lp --save-crop --save-vid  --save-txt --strong-sort-weights weights/osnet_x0_25_msmt17.pt --yolo-weights weights/yolov7.pt"
+executable      = detect_lp.py
+arguments       = "-python detect_lp.py --source ../../test/20230222_115854.mp4 --weights ../weights/yolov7.pt --conf 0.25 --save-txt --save-conf --device 0 --classes 1 2 3 5 7"
 output          = lp_yolo_ocr_output.txt
 log             = lp_yolo_ocr_log.log
 error 		= lp_yolo_ocr_error.log
